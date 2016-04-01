5124e951894bbafcdf2c21701ed997764b908482
nayakrahul <rahulnk521@gmail.com>
2016-04-01 12:24:05 +0530

    switch to course layout

---
+++ app/src/main/java/com/github/amitkmr/attendencemarker/Course.java
@@ -0,0 +1,19 @@
+package com.github.amitkmr.attendencemarker;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+/**
+ * Created by RAHUL on 01-04-2016.
+ */
+public class Course extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_courses);
+    }
+}
