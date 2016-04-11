99cf56081da4a7567559e961989c71c6648842a2
Amit Kumar <itskmramit@gmail.com>
2016-04-12 01:52:11 +0530

    final UI changes of attendance page

---
+++ app/src/main/java/com/github/amitkmr/attendencemarker/AttendanceDetails.java
@@ -0,0 +1,48 @@
+package com.github.amitkmr.attendencemarker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class AttendanceDetails extends AppCompatActivity {
+    private DBHelper mydb ;
+    private TextView desc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_details);
+
+        Intent intent = getIntent();
+        String id = intent.getExtras().getString("id");
+        mydb = new DBHelper(this);
+
+        String data;
+        data = mydb.getData(id);
+
+//        desc = (TextView) findViewById(R.id.description);
+//        desc.setText(data);
+
+        // return to to the home screen
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        //set titile in the app bar
+        setTitle("Attendance Details");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; goto parent activity.
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
